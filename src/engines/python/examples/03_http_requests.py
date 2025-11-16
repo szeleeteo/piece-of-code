8b1b39290d4093fe2f876016978bb192f822f365
szelee <szelee@temasek.com.sg>
2025-11-16 21:34:01 +0800

    Remove Streamlit dependency and API key handling from HTTP requests example

--- src/engines/python/examples/03_http_requests.py
+++ src/engines/python/examples/03_http_requests.py
@@ -1,11 +1,8 @@
 import requests
-import streamlit as st
 
 DATA_GOV_SG_API_KEY = "https://api.data.gov.sg"
-TOKEN = st.secrets["DATA_GOV_SG_API_KEY"]
 
 url = f"{DATA_GOV_SG_API_KEY}/v1/environment/24-hour-weather-forecast"
-headers = {"X-Api-Key": TOKEN}
-response = requests.get(url, headers=headers)
+response = requests.get(url)
 
 print(response.json())
